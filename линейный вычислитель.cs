4f7d88ef4ebc2804b912e4acd68e4b1a74d11ba6
Siber497 <siber497@gmail.com>
2024-07-13 20:53:34 +0300

    dobavil domasniy proekt

---
+++ линейный вычислитель.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace elmir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string year;
+
+            short a, b;
+
+            Console.WriteLine("Здраствуйте, введите пожалуйста год вашего рождения:  ");
+
+            year = Console.ReadLine();
+
+            a = Convert.ToInt16(year);
+
+            b = 2024;
+
+            short age = Convert.ToInt16 (b - a);
+
+            Console.WriteLine($"Ваш возраст составляет {age} года.");
+            Console.WriteLine("Спасибо что использовали нашу прошрамму!!!");
+
+        }
+    }
+}
